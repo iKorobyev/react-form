09355dd1a8b85cd2ee35fd25387785a6ff0c6104
Igor <iKorobyev@google.com>
2021-06-12 20:33:07 +0300

    Start developing react-form.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div className="wrapper">
+      <div className="container">
+        <header className="header">
+          <h1 className="header__title">Персональные данные.</h1>
+        </header>
+        <div className="content">
+            <h2 className="content__title">О себе:</h2>
+            <div className="content__items">
+                <div className="item__input">
+                    <form onSubmit={''}>
+                        <input type="text" placeholder='Фамилия*' name='name' />
+                        <input type="text" placeholder='Имя*' name='name' />
+                        <input type="text" placeholder='Отчество' name='name' />
+                        <input type="text" placeholder='Дата рождения*' name='name' />
+                        <input type="text" placeholder='Номер телефона*' name='name' />
+                    </form>
+                </div>
+                <div className="item__input">2</div>
+                <div className="item__input">3</div>
+            </div>
+        </div>
+      </div>
     </div>
-  );
 }
 
 export default App;
